6aeab13140cddf05a7b00245c759a628ef7843dc
Elizabeth <elizbregman@gmail.com>
2020-07-16 22:54:49 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -22,8 +22,9 @@ function incrementVariable() {
   i++;
   return i;
 }
+
 function doWhileLoop(num) {
     do {
       console.log("I run once regardless");
-    } while (incrementVariable() < num);
+    } while (incrementVariable() <= num);
 }
